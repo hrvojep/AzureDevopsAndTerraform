d46f2df47a8a8e5b4df3913930f1ad5f9ff0597d
H <hrvojep@yahoo.com>
2021-09-04 19:01:40 +1000

    second commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {    
+    features {}
+}
+
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg-h"
+  location = "australiaeast"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "h-weatherapi"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "hrvoje/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
